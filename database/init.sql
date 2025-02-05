216b0be6fc46fc8e6f26e19c61a1fe555ddc6d0e
ajimz12 <alvarojimgil12@gmail.com>
2025-02-05 12:31:33 +0100

    REFACTOR: docker-compose

--- database/init.sql
+++ database/init.sql
@@ -2,4 +2,4 @@ CREATE TABLE secretosAJG (
  id INT AUTO_INCREMENT PRIMARY KEY,
  fraseAJG VARCHAR(255) NOT NULL
 );
-INSERT INTO secretosAJG (fraseAJG) VALUES ('La base de datos responde, buenos días Alvaro Jimenez');
+INSERT INTO secretosAJG (fraseAJG) VALUES ('La base de datos responde, buenos dias Alvaro Jimenez');
